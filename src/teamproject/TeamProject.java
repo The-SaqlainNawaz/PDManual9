e29feac79ccde702e9b89825fb5d909102d0af46
Mumer666 <81896836+Mumer666@users.noreply.github.com>
2021-04-23 23:27:15 -0700

    wordcounter

--- src/teamproject/TeamProject.java
+++ src/teamproject/TeamProject.java
@@ -13,6 +13,20 @@ import java.util.Scanner;
  */
 public class TeamProject {
 
+        public static int wordCounter(String s)
+    {
+        int wordCount=1;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            if(s.charAt(i)!=' ' &&  s.charAt(i+1)==' ')
+            {
+                wordCount++;
+            }
+        }
+        
+        return wordCount;
+    }
+        
     public static int vowelcounter(String s) {
         String trim = s.trim();
         String vowels = "aeiouAEIOU";
@@ -36,6 +50,8 @@ public class TeamProject {
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter String :");
         String str = obj.nextLine();
+        int number=wordCounter(str);
+        System.out.println(" no of words in the string is = "+number);
         int counter=vowelcounter(str);
         System.out.println("Vowel counts :" + counter);
     }

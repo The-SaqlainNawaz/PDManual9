e38c5674b10c93fe8d59ec16248ecf456f58e93d
The-SaQii <pacsaqiirr@gmail.com>
2021-04-24 13:04:14 +0500

    Task1 +Task2

--- src/teamproject/Task1.java
+++ src/teamproject/Task1.java
@@ -20,7 +20,6 @@ public class Task1 {
                 wordCount++;
             }
         }
-
         return wordCount;
     }
 
--- src/teamproject/Task2.java
+++ src/teamproject/Task2.java
@@ -4,30 +4,31 @@
  * and open the template in the editor.
  */
 package teamproject;
+
 import java.util.Scanner;
+
 public class Task2 {
 
-    
-      public static void displayVertical(String sent)
-    {
-        for(int i=0;i<sent.length();i++)
-        {
-            if(sent.charAt(i)!=' ')
-            {
-                System.out.println(sent.charAt(i));
-            }
+    public static String displayVertical(String sent) {
+        String out = "";
+        for (int i = 0; i < sent.length(); i++) {
+           if (sent.charAt(i) != ' ') {            
+                out = out + sent.charAt(i) + "\n";
+
+           }
         }
-        
+        return out;
     }
+
     public static void main(String[] args) {
         // TODO code application logic here
-            Scanner obj=new Scanner(System.in);
+        Scanner obj = new Scanner(System.in);
         String sentence;
-        System.out.println(" please enter a string ");
-        sentence=obj.nextLine();
-        System.out.println(" your string presented vertically is ");
-        displayVertical(sentence);
-    
+        System.out.print(" Please enter a sentence: ");
+        sentence = obj.nextLine();
+        System.out.println(" Your string presented vertically is ");
+        String out=displayVertical(sentence);
+        System.out.println(out);
     }
-    
+
 }

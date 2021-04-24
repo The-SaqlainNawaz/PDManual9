6ad0e3b85a4405c74c01901b87b618af906c19fb
The-SaQii <pacsaqiirr@gmail.com>
2021-04-24 11:17:02 +0500

    VowelCounter

--- src/teamproject/TeamProject.java
+++ src/teamproject/TeamProject.java
@@ -5,17 +5,39 @@
  */
 package teamproject;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Proud To Be Muslims
  */
 public class TeamProject {
 
+    public static int vowelcounter(String s) {
+        String trim = s.trim();
+        String vowels = "aeiouAEIOU";
+        int countedVowels = 0;
+        for (int i = 0; i < trim.length(); i++) {
+            for (int j = 0; j < vowels.length(); j++) {
+                if (trim.charAt(i) == (vowels.charAt(j))) {
+                    countedVowels++;
+                    break;
+                }
+            }
+        }
+        return countedVowels;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter String :");
+        String str = obj.nextLine();
+        int counter=vowelcounter(str);
+        System.out.println("Vowel counts :" + counter);
     }
-    
+
 }

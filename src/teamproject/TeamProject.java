65d47280561c9da9cce3b5f125e93d6f1ab0520e
The-SaQii <pacsaqiirr@gmail.com>
2021-04-24 12:00:59 +0500

    Punct counter

--- src/teamproject/TeamProject.java
+++ src/teamproject/TeamProject.java
@@ -13,20 +13,17 @@ import java.util.Scanner;
  */
 public class TeamProject {
 
-        public static int wordCounter(String s)
-    {
-        int wordCount=1;
-        for(int i=0;i<s.length()-1;i++)
-        {
-            if(s.charAt(i)!=' ' &&  s.charAt(i+1)==' ')
-            {
+    public static int wordCounter(String s) {
+        int wordCount = 1;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) != ' ' && s.charAt(i + 1) == ' ') {
                 wordCount++;
             }
         }
-        
+
         return wordCount;
     }
-        
+
     public static int vowelcounter(String s) {
         String trim = s.trim();
         String vowels = "aeiouAEIOU";
@@ -42,6 +39,20 @@ public class TeamProject {
         return countedVowels;
     }
 
+    public static int intpuncCount(String s) {
+        String trim = s.trim();
+        String punct = ".,?!;:'-/";
+        int punccounter = 0;
+        for (int i = 0; i < trim.length(); i++) {
+            for (int j = 0; j < punct.length(); j++) {
+                if (trim.charAt(i) == punct.charAt(j)) {
+                    punccounter++;
+                }
+            }
+        }
+        return punccounter;
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -50,10 +61,12 @@ public class TeamProject {
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter String :");
         String str = obj.nextLine();
-        int number=wordCounter(str);
-        System.out.println(" no of words in the string is = "+number);
-        int counter=vowelcounter(str);
-        System.out.println("Vowel counts :" + counter);
+        int number = wordCounter(str);
+        System.out.println("Number of words  = " + number);
+        int counter = vowelcounter(str);
+        System.out.println("Number of vowels =" + counter);
+        int puncoutere = intpuncCount(str);
+        System.out.println("Number of punctuations =" + puncoutere);
     }
 
 }

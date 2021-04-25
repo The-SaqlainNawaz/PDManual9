61e9f93a6ebcae68bc4c26d700cf9dd878b7b232
The-SaQii <pacsaqiirr@gmail.com>
2021-04-25 11:12:46 +0500

    Task 3 Completed

--- src/teamproject/Task3.java
+++ src/teamproject/Task3.java
@@ -5,32 +5,90 @@
  */
 package teamproject;
 
- import java.util.Scanner;
+import java.util.Random;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import static teamproject.NewMain.stringEncryption;
+
 public class Task3 {
 
-     public static String removeInteger( String s)
-    {
-        String out="";
-        for(int i=0;i<s.length();i++)
-        {
-            if(!(s.charAt(i)>='0'  &&  s.charAt(i)<='9'))
-            {
-                out=out+s.charAt(i);
-                
+    public static String removeInteger(String s) {
+        String out = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (!(s.charAt(i) >= '0' && s.charAt(i) <= '9')) {
+                out = out + s.charAt(i);
+
             }
         }
-        
+
         return out;
     }
+
+    public static String ridMultipleBlank(String s) {
+        String out = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (!(s.charAt(i) == ' ' && s.charAt(i + 1) == ' ')) {
+                out = out + s.charAt(i);
+
+            }
+        }
+
+        return out;
+    }
+
+    public static String stringEncryption(String s) {
+        int size = s.length();
+        Random rand = new Random();
+        String alphaNumericCharacters = "abcdefghijklmnopqrstuvwxyz"
+                + "ABCDEFGHIJLMNOPQRSTUVWXYZ"
+                + "1234567890";
+
+        // Use StringBuilder in place of String to avoid unnecessary object formation
+        String result = new String();
+        result = "";
+        if (size >= 8) {
+            for (int i = 0; i < size / 4; i++) {
+                result = result + (alphaNumericCharacters.charAt(rand.nextInt(alphaNumericCharacters.length())));
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                result = result + (alphaNumericCharacters.charAt(rand.nextInt(alphaNumericCharacters.length())));
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
-         Scanner obj=new Scanner(System.in);
+        Scanner obj = new Scanner(System.in);
         String sentence;
-        System.out.print(" enter a string   ");
-       sentence=obj.nextLine();
-       String remove;
-       remove=removeInteger(sentence);
-       System.out.println(" string without integer is  "+remove);
+        int input = -1;
+        do {
+            String choose = JOptionPane.showInputDialog("Features" + "\n" + "Press 1.To Use ridMultipleBlank Method " + "\n" + "Press 2.To use removeInteger Method " + "\n" + "Press 3.To use stringEncryption Method" + "\n" + "Press 0.To Exit");
+            input = Integer.parseInt(choose);
+            switch (input) {
+                case 1:
+                    sentence = JOptionPane.showInputDialog("Enter a string: ");
+                    String out = ridMultipleBlank(sentence);
+                    JOptionPane.showMessageDialog(null, "Output:" + "\n" + out);
+
+                    break;
+                case 2:
+                    sentence = JOptionPane.showInputDialog("Enter a string: ");
+                    String remove;
+                    remove = removeInteger(sentence);
+                    JOptionPane.showMessageDialog(null, "Output:" + "\n" + remove);
+                    break;
+                case 3:
+                    sentence = JOptionPane.showInputDialog("Enter a string: ");
+                    String encrp;
+                    encrp = stringEncryption(sentence);
+                    JOptionPane.showMessageDialog(null, "Output:" + "\n" + encrp);
+                    break;
+                case 0:
+                    System.exit(0);
+            }
+        } while (input != 0);
     }
-    
+
 }
